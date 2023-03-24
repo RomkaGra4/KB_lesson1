a0791aeb4f2d20d84acd0f8bae7138423ca270e2
romka <romka_gra4@mail.ru>
2023-03-25 01:55:26 +0500

    KS-1-2: completed the second task of the first lesson

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main (){
+
+    val theNumberOfOrders: Byte = 75
+    val textForClient: String = "Спасибо за покупку!"
+
+    println(theNumberOfOrders)
+    println(textForClient)
+
+    var theNumberOfEmployees: UShort = 2000u
+
+    //println(theNumberOfEmployees)
+    theNumberOfEmployees = 1999u
+
+    println(theNumberOfEmployees)
+
+}
