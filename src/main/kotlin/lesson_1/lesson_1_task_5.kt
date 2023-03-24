bc82c15a4b9f4950b798262da2fb121ec90b6623
romka <romka_gra4@mail.ru>
2023-03-25 03:16:27 +0500

    KS-1-5: completed the fifth task of the first lesson

---
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+    fun main(){
+
+        var second  = 6480
+        var minute = second / 60
+        var second2= second % 60
+
+        println("$minute:$second2")
+
+
+    }
