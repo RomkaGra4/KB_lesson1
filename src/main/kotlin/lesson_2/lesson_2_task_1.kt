86ab2a1da76ba9cc36a490272e1e237219305d2d
romka <romka_gra4@mail.ru>
2023-03-29 00:50:48 +0500

    KS-2-1: completed the firth task of the SECOND lesson

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,11 @@
+package lesson_2
+
+
+
+fun main (){
+
+        var GPA = (3 + 4 + 3 + 5) / 4.0 // GPA - средний балл (Grade Point Average)
+        var result = String.format("%.2f",GPA)
+        println(result)
+
+}
