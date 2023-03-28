46f71be12eedc69f48f6b03c4f496b618f17cde2
romka <romka_gra4@mail.ru>
2023-03-29 01:03:56 +0500

    KS-2-2: completed the second task of the SECOND lesson

---
+++ src/main/kotlin/lesson_2/lesson_2_task_2.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+    fun main (){
+
+        var employeeSalary = 30000 * 50
+        println(employeeSalary)
+
+        var totalSalary = employeeSalary + (20000 * 30)
+        println(totalSalary)
+
+        var averageEmployeeSalary = totalSalary / 80
+        println(averageEmployeeSalary)
+
+    }
