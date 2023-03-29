2825f1d8d455ab0a04663f2dde589fded2fb44b2
romka <romka_gra4@mail.ru>
2023-03-30 03:40:30 +0500

    KS-3-4: completed the fourth task of the third lesson

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,13 @@
+package lesson_3
+
+    fun main(){
+
+        var stepOld = "E2"
+        var stepNew = "E4"
+        var counter = 1
+            println("$stepOld-$stepNew;" + counter++)
+
+        stepOld = "D2"
+        stepNew = "D3"
+            println("$stepOld-$stepNew;" + counter++)
+    }
