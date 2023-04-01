9399267368ae0fdc45666c8e9a40ed24c8c60c47
romka <romka_gra4@mail.ru>
2023-04-01 06:11:25 +0500

    KS-3-4:  corrections made

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -1,13 +1,13 @@
 package lesson_3
 
-    fun main(){
+fun main() {
 
-        var stepOld = "E2"
-        var stepNew = "E4"
-        var counter = 1
-            println("$stepOld-$stepNew;" + counter++)
+    var stepOld = "E2"
+    var stepNew = "E4"
+    var counter = 0
+    println("$stepOld-$stepNew; ${++counter}")
 
-        stepOld = "D2"
-        stepNew = "D3"
-            println("$stepOld-$stepNew;" + counter++)
-    }
+    stepOld = "D2"
+    stepNew = "D3"
+    println("$stepOld-$stepNew; ${++counter}")
+}
