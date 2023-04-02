5e6ed200c1fa0da2e7b8329019b784ec48fe0fd7
romka <romka_gra4@mail.ru>
2023-04-03 00:20:33 +0500

    KS-6-4: completed the fourth task of the SIXTH lesson

---
+++ src/main/kotlin/lesson_6/lesson_6_task_4.kt
@@ -0,0 +1,15 @@
+package lesson_6
+
+fun main() {
+
+    val randomNumber = (1..9).random()
+    var attempt = 5
+
+    println("Введите одно число от 1 до 9: ")
+    do {
+        val userNumber = readln().toInt()
+        if (userNumber == randomNumber)
+            println("Это была великолепная игра!")
+        attempt--
+    } while (randomNumber != userNumber && attempt != 0)
+}
