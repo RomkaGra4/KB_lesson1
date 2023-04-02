e621aff0bc0a47ef7c406d7536d8e3d1855b410d
romka <romka_gra4@mail.ru>
2023-04-02 22:23:17 +0500

    KS-5-1: removed the magic number

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -2,10 +2,11 @@ package lesson_5
 
 fun main(){
 
+    val sum = 9 + 6
     println("Для входа в программу, укажите сумму двух чисел: 9 и 6.")
-    val result = readLine()!!.toInt()
+    val result = readln().toInt()
 
-    if (result == 15)
+    if (result == sum)
         println("Добро пожаловать!")
     else
         println("Доступ запрещен.")
