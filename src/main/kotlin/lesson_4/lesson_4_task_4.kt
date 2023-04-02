6a607b104818d4ba09353ffcb40fe1df5ace580c
romka <romka_gra4@mail.ru>
2023-04-02 21:49:56 +0500

    KS-4-4: corrected to "not winter"

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -3,13 +3,16 @@ package lesson_4
 fun main() {
 
     val trainingDay = 5
+    val armAndAbsExercises = trainingDay % 2 != 0
+    val legAndBackExercises = trainingDay % 2 == 0
 
     println(
         """
-        Упражнения для рук:    ${trainingDay % 2 != 0}
-        Упражнения для ног:    ${trainingDay % 2 == 0}   
-        Упражнения для спины:  ${trainingDay % 2 == 0}
-        Упражнения для пресса: ${trainingDay % 2 != 0}
+        Упражнения для рук:    $armAndAbsExercises 
+        Упражнения для ног:    $legAndBackExercises   
+        Упражнения для спины:  $legAndBackExercises
+        Упражнения для пресса: $armAndAbsExercises
     """.trimIndent()
     )
+
 }
