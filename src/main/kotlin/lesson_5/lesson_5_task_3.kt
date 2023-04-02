b354b87c27a490a5a7b07acc9bbb1d1ce6c2a3b3
romka <romka_gra4@mail.ru>
2023-04-02 22:30:05 +0500

    KS-5-3: adjustment - DONE

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -1,17 +1,18 @@
 package lesson_5
 
-fun main(){
-
-    println("Введите два числа (от 1 до 100):")
-    val firstUserNumber = readLine()!!.toInt()
-    val secondUserNumber = readLine()!!.toInt()
+fun main() {
 
     val firstNumber = 45
     val secondNumber = 73
 
-    val result = if (firstUserNumber == firstNumber && secondUserNumber == secondNumber) "Поздравляем! Вы выиграли главный приз!"
-    else if (firstUserNumber == firstNumber || secondUserNumber == secondNumber) "Вы выиграли утешительный приз!"
-    else "Неудача! Крутите барабан!"
+    println("Введите два числа (от 1 до 100):")
+    val firstUserNumber = readln().toInt()
+    val secondUserNumber = readln().toInt()
+
+    val result =
+        if (firstUserNumber == firstNumber && secondUserNumber == secondNumber) "Поздравляем! Вы выиграли главный приз!"
+        else if (firstUserNumber == firstNumber || secondUserNumber == secondNumber) "Вы выиграли утешительный приз!"
+        else "Неудача! Крутите барабан!"
 
     println(result)
     println("Числа, необходимые для победы: $firstNumber и $secondNumber")
