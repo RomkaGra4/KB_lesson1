fa08ac03b08fe522b08acdd141372476c4f4f305
romka <romka_gra4@mail.ru>
2023-04-05 22:06:22 +0500

    KS-7-2: completed the second task of the Seventh lesson

---
+++ src/main/kotlin/lesson_7/lesson_7_task_2.kt
@@ -0,0 +1,19 @@
+package lesson_7
+
+fun main(){
+
+
+    do {
+        val randomCode = (1000..9999).random()
+        println("Ваш код авторизации: $randomCode")
+
+        println("Введите КОД для авторизации: ")
+        val userCode = readln().toInt()
+            if (userCode != randomCode) {
+                continue
+            } else {
+                println("КОД введен верно! Приветствуем Вас! ;)")
+            }
+    } while (userCode != randomCode)
+
+}
