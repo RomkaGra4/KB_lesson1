3ed58653294f2d012f5745b7b236f17f3b502eab
romka <romka_gra4@mail.ru>
2023-04-06 03:11:20 +0500

    KS-4-4: add correct (isEvenDay)

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -3,14 +3,14 @@ package lesson_4
 fun main() {
 
     val trainingDay = 5
-    val evenNumber = trainingDay % 2 == 0
+    val isEvenDay = trainingDay % 2 == 0
 
     println(
         """
-        Упражнения для рук:    ${!evenNumber} 
-        Упражнения для ног:    $evenNumber  
-        Упражнения для спины:  $evenNumber 
-        Упражнения для пресса: ${!evenNumber}
+        Упражнения для рук:    ${!isEvenDay} 
+        Упражнения для ног:    $isEvenDay  
+        Упражнения для спины:  $isEvenDay 
+        Упражнения для пресса: ${!isEvenDay}
     """.trimIndent()
     )
 }
