7a0d663bc61f94ae243ff6369952bdf7b26e623e
romka <romka_gra4@mail.ru>
2023-04-06 22:52:40 +0500

    KS-7-1: with correct

--- src/main/kotlin/lesson_7/lesson_7_task_1.kt
+++ src/main/kotlin/lesson_7/lesson_7_task_1.kt
@@ -4,17 +4,11 @@ fun main() {
 
     val charsRange = ('0'..'9') + ('a'..'z')
 
-   /* val symnolOne = (charsRange).random().lowercase()
+    val symnolOne = (charsRange).random().lowercase()
     val symnolTwo = (charsRange).random().lowercase()
     val symnolThree = (charsRange).random().lowercase()
     val symnolFour = (charsRange).random().lowercase()
     val symnolFive = (charsRange).random().lowercase()
     val symnolSix = (charsRange).random().lowercase()
-    val password = symnolOne + symnolTwo + symnolThree + symnolFour + symnolFive + symnolSix
-    println(password)
-*/
-    for (i in 1..6) {
-        val i = (charsRange).random().lowercaseChar()
-        print(i)
-    }
+    println("$symnolOne$symnolTwo$symnolThree$symnolFour$symnolFive$symnolSix")
 }
