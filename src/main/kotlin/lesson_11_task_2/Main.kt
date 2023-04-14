1b45f74a23992fbab3873e0310d7986fefff3616
romka <romka_gra4@mail.ru>
2023-04-15 01:44:22 +0500

    Kotlin Sprint: lesson 11, task 2 (UserData теперь рядом с функцией MAIN + исправил опечатку)

--- src/main/kotlin/lesson_11_task_2/Main.kt
+++ src/main/kotlin/lesson_11_task_2/Main.kt
@@ -1,6 +1,6 @@
 package lesson_11_task_2
 
-fun main(){
+fun main() {
     val user = UserData(
         id = 1,
         login = "Роман",
@@ -12,4 +12,45 @@ fun main(){
     user.getInformationAndSaveInBio()
     user.changePassword()
     user.getTextForSend()
+}
+
+class UserData(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    val bio: String = "",
+) {
+
+    fun printUserInformation() {
+        println(
+            """"Данные пользователя:
+    ID - $id
+    Логин - $login
+    Пароль - $password
+    Email - $email
+    """.trimIndent()
+        )
+    }
+
+    fun getInformationAndSaveInBio() {
+        println("Введите информацию о себе:")
+        val bio = readln()
+    }
+
+    fun changePassword() {
+        println("Введите текущий пароль:")
+        if (password == readln()) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Пароль изменен!")
+        }
+    }
+
+    fun getTextForSend() {
+        println("Введите текст, который мы отправим пользователю:")
+        println("Дорогой $login, ${readln()}")
+        println("*****ПИСЬМО ОТПРАВЛЕНО!*****")
+    }
+
 }
