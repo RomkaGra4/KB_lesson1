bc96da58d76a0f760f870187f5d826427d8572f8
romka <romka_gra4@mail.ru>
2023-04-15 13:38:34 +0500

    Kotlin Sprint: Lesson 11, task 3

---
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -0,0 +1,34 @@
+package lesson_11
+
+fun main() {
+    val room1 = Room(
+        cover = "Синяя",
+        name = "Книжный клуб",
+        listOfParticipants = listOf(
+            User(name = "Роман", userStatus = "разговаривает"),
+            User(name = "Ольга", userStatus = "микрофон выключен")
+        )
+    )
+
+    val room2 = Room(
+        cover = "Красная",
+        name = "Приготовление блюд",
+        listOfParticipants = listOf(
+            User(name = "Анна", userStatus = "микрофон выключен"),
+            User(name = "Дмитрий", userStatus = "пользователь заглушен")
+        )
+    )
+
+    val roomsList = listOf(room1, room2)
+}
+
+class Room(
+    val cover: String,
+    val name: String,
+    val listOfParticipants: List<User>,
+) {}
+
+class User(
+    val name: String,
+    val userStatus: String,
+) {}
