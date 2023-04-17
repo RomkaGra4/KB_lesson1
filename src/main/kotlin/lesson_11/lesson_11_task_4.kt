f32096d7529563e719c0a1be36a189c1ade868a9
romka <romka_gra4@mail.ru>
2023-04-17 19:17:51 +0500

    Kotlin Sprint: Lesson 11, task 3 (добавил свойство с типом "список объектив типа FavoriteContact" )

--- src/main/kotlin/lesson_11/lesson_11_task_4.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_4.kt
@@ -1,30 +1,30 @@
 package lesson_11
 
-fun main(){
+fun main() {
     val contact = Contact(
         number = TelephoneNumber(mobilePhone = 89995150033, cityPhone = 84953991010),
         emailAdress = ICloud("mail@mail.ru"),
-        myPeople = FavoriteContacts(wife = "Username", friend1 = "Username", friend2 = "Username")
+        myPeople = listOf(FavoriteContact(wife = "Username", friend1 = "Username", friend2 = "Username"))
     )
 }
 
 class Contact(
     val number: TelephoneNumber,
     val emailAdress: ICloud,
-    val myPeople: FavoriteContacts,
-){}
+    val myPeople: List<FavoriteContact>,
+)
 
 class TelephoneNumber(
     val mobilePhone: Long,
     val cityPhone: Long,
-){}
+)
 
 class ICloud(
     val email: String,
-){}
+)
 
-class FavoriteContacts(
+class FavoriteContact(
     val wife: String,
     val friend1: String,
     val friend2: String,
-){}
+)
