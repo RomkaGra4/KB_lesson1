7c9588e61360a390cf6fe7446cffc2254730bea6
romka <romka_gra4@mail.ru>
2023-04-21 19:58:20 +0500

    KS 13 - 2: Completed task number 2 for lesson 13 (Добавил проверку на наличие null)

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -2,15 +2,19 @@ package lesson_13
 
 fun main() {
 
-    val contact = PhoneBook("Роман", 87775530012, )
-    contact.printInformation()
+    val contact = PhoneBook("Роман", 87775530012, null)
 
+    if (contact != null) {
+        if (contact.company == null) contact.company = "[не указано]"
+    }
+
+    contact?.printInformation()
 }
 
 class PhoneBook(
     val name: String,
     val number: Long,
-    val company: String? = null ?: "[не указано]",
+    var company: String? = null ?: "[не указано]",
 ){
     fun printInformation(){
         println("Имя: $name \nНомер: $number \nКомпания: $company")
