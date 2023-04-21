7eb896a4a8b04e3bda3ca5510e622d52f7af64d4
romka <romka_gra4@mail.ru>
2023-04-21 20:07:52 +0500

    KS 13 - 3: Completed task number 3 for lesson 13 (Добавил проверку на наличие null )

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -2,9 +2,10 @@ package lesson_13
 
 fun main() {
 
-    val contact1 = TelephoneBook(name = "Роман", company = "FM Logistic")
-    val contact2 = TelephoneBook(name = "Ольга", number = "89605537705")
+    val contact1 = TelephoneBook(name = "Роман", number = null, company = "FM Logistic")
+    val contact2 = TelephoneBook(name = "Ольга", number = "89605537705", company = null)
     val contact3 = TelephoneBook(number = "89165520202", company = "Pepsi Co")
+
 
     val contactsList = listOf(contact1, contact2, contact3)
     printInformation(contactsList)
@@ -12,12 +13,18 @@ fun main() {
 }
 
 class TelephoneBook(
-    val name: String? = null ?: "[не указано]",
-    val number: String? = null ?: "[не указано]",
-    val company: String? = null ?: "[не указано]",
+    var name: String? = null,
+    var number: String? = null,
+    var company: String? = null,
 )
 
 fun printInformation(contactsList: List<TelephoneBook>) {
+
+    for (i in contactsList) {
+        if (i.name == null) i.name = "[не указано]"
+        if (i.number == null) i.number = "[не указано]"
+        if (i.company == null) i.company = "[не указано]"
+    }
 
     for (element in contactsList)
         println("Имя: ${element.name} \nНомер: ${element.number} \nКомпания: ${element.company} \n")
