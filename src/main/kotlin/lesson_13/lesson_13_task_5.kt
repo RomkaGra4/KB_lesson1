6ff62a37527e2781f81e4adbcd41097dacfdf711
romka <romka_gra4@mail.ru>
2023-04-21 21:51:15 +0500

    KS 13 - 5: Completed task number 5 for lesson 13

---
+++ src/main/kotlin/lesson_13/lesson_13_task_5.kt
@@ -0,0 +1,15 @@
+package lesson_13
+
+fun main() {
+
+    val number: Long
+    val strNumber = readLine().toString()
+    try {
+        number = strNumber.toLong()
+        println(number)
+    } catch (e: Exception) {
+        println("Произошла ошибка! Номер может содержать только цифры!")
+    }
+
+}
+
