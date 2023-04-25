990fc22bcb670233c7193ed5766ab3730f501ddb
romka <romka_gra4@mail.ru>
2023-04-25 21:46:54 +0500

    KS 13 - 3: Completed task number 3 for lesson 13

--- src/main/kotlin/lesson_13/lesson_13_task_3.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -2,23 +2,22 @@ package lesson_13
 
 fun main() {
 
-    val contact1 = TelephoneBook(name = "Роман", number = null, company = "FM Logistic")
-    val contact2 = TelephoneBook(name = "Ольга", number = "89605537705", company = null)
-    val contact3 = TelephoneBook(number = "89165520202", company = "Pepsi Co")
-
+    val contact1 = ContactOnTelephoneBook(name = "Роман", number = null, company = "FM Logistic")
+    val contact2 = ContactOnTelephoneBook(name = "Ольга", number = "89605537705", company = null)
+    val contact3 = ContactOnTelephoneBook(number = "89165520202", company = "Pepsi Co")
 
     val contactsList = listOf(contact1, contact2, contact3)
     printInformation(contactsList)
 
 }
 
-class TelephoneBook(
+class ContactOnTelephoneBook(
     var name: String? = null,
     var number: String? = null,
     var company: String? = null,
 )
 
-fun printInformation(contactsList: List<TelephoneBook>) {
+fun printInformation(contactsList: List<ContactOnTelephoneBook>) {
 
     for (i in contactsList) {
         if (i.name == null) i.name = "[не указано]"
