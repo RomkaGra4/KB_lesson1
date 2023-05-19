be272b72fa9ced1abc10c9e58b5ffa0c4ff2877c
romka <romka_gra4@mail.ru>
2023-05-19 20:52:55 +0500

    KS 14 - 3: Создал 2 объекта, перенес метод в род. класс

--- src/main/kotlin/lesson_14/lesson_14_task_3.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_3.kt
@@ -1,27 +1,28 @@
 package lesson_14
 
 fun main() {
-    val author1 = Comment("Привет! Как дела?", "Роман")
-    author1.sendMessage()
-    author1.sendComment()
+    val message = MessageInForum("Привет! Как дела?", "Роман")
+    val comment = Comment(message.text, "Ольга", "Спасибо, хорошо!")
+
+    message.sendMessage()
+    comment.sendComment()
 }
 
 open class MessageInForum(
     val text: String,
     val author: String,
-) {}
+) {
+    fun sendMessage() {
+        println("Автор \"$author\" отправил следующее сообщение: \"$text\"")
+    }
+}
 
 class Comment(
     text: String,
     author: String,
+    val comment: String,
 ) : MessageInForum(text, author) {
-
-    fun sendMessage() {
-        println("Автор \"$author\" отправил следующее сообщение: \"$text\"")
-    }
-
     fun sendComment() {
-        println("Автор \"$author\" оставил комментарий над сообщением \"$text\": \"${readln()}\"")
+        println("Автор \"$author\" оставил комментарий над сообщением \"$text\": \"$comment\"")
     }
 }
-
