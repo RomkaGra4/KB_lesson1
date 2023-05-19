a039531bf6245f32a5e96e265fd0bc677f82acf1
romka <romka_gra4@mail.ru>
2023-05-19 23:53:28 +0500

    KS 14 - 4: Completed task number 4 for lesson 14

---
+++ src/main/kotlin/lesson_14/lesson_14_task_4.kt
@@ -0,0 +1,41 @@
+package lesson_14
+
+fun main(){
+
+    val sputnik1 = Sputnik("Фобос", false,false,false,true)
+    val sputnik2 = Sputnik("Деймос", false,false,false,true)
+    val planet = Planet("Марс", false,false,true,true, listOf(sputnik1.name, sputnik2.name))
+
+    planet.printInfo()
+
+}
+
+open class HeavenlyBody(
+    val inhabited: Boolean,
+    val atmosphere: Boolean,
+    val water: Boolean,
+    val suitableForLanding: Boolean,
+)
+
+class Planet(
+    val name: String,
+    inhabited: Boolean,
+    atmosphere: Boolean,
+    water: Boolean,
+    suitableForLanding: Boolean,
+    val sputniks: List<String>,
+) : HeavenlyBody(inhabited, atmosphere, water, suitableForLanding){
+
+    fun printInfo(){
+        println("Платена $name имеет следующие спутники: $sputniks")
+
+    }
+}
+
+class Sputnik(
+    val name: String,
+    inhabited: Boolean,
+    atmosphere: Boolean,
+    water: Boolean,
+    suitableForLanding: Boolean,
+) : HeavenlyBody(inhabited, atmosphere, water, suitableForLanding)
