34edbbae7fd25ff6e648e22466a99bd2759afc6d
romka <romka_gra4@mail.ru>
2023-06-30 23:38:38 +0500

    KS 16 - 2:  Completed task number 2 for lesson 16

---
+++ src/main/kotlin/lesson_16/lesson_16_task_2.kt
@@ -0,0 +1,25 @@
+package lesson_16
+
+import kotlin.math.pow
+
+private const val PI = 3.14
+
+fun main() {
+    val result = Circle(5)
+    result.printCircumference()
+    result.printAreaOfACircle()
+}
+
+class Circle(private val radius: Int) {
+
+    fun printCircumference() {
+        val circumference = PI * (radius * 2)
+        println(circumference.toFloat())
+    }
+
+    fun printAreaOfACircle() {
+        val square = PI * (radius.toDouble().pow(2.0))
+        println(square.toFloat())
+    }
+}
+
