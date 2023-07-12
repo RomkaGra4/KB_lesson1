bb4848b6bb3dda9a5d0bfd9c0c51bb6cccf1d9ec
romka <romka_gra4@mail.ru>
2023-07-13 00:50:59 +0500

    KS 17 - 5:  Completed task number 5 for lesson 17

---
+++ src/main/kotlin/lesson_17/lesson_17_task_5.kt
@@ -0,0 +1,22 @@
+package lesson_17
+
+fun main(){
+    val player1 = Player("Roman", "qwerty123")
+    player1.password = "asdfg123"
+    println(player1.password)
+    player1.login = "Roman1993"
+    println(player1.login)
+
+}
+
+class Player(_login: String, _password: String){
+    var login = _login
+        set(value) {
+            println("Ваш логин успешно изменен")
+            field = value
+        }
+    var password = _password
+        set(value) {
+            println("Вы не можете изменить пароль")
+        }
+}
