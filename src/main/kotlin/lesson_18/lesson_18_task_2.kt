d390d0819eb3753bcf450d18e683a8a8cb126aee
romka <romka_gra4@mail.ru>
2023-07-19 00:47:29 +0500

    KS 18 - 2:  Completed task number 2 for lesson 18

---
+++ src/main/kotlin/lesson_18/lesson_18_task_2.kt
@@ -0,0 +1,42 @@
+package lesson_18
+
+fun main(){
+    val dice1:FourSidedDice = FourSidedDice()
+    val dice2:SixSidedDice = SixSidedDice()
+    val dice3:EightSidedDice = EightSidedDice()
+
+    println(dice1.getNumberDice())
+    println(dice2.getNumberDice())
+    println(dice3.getNumberDice())
+
+}
+
+open class Dice {
+
+    open fun getNumberDice() = ""
+
+}
+
+class FourSidedDice(private val number: Int = 4): Dice(){
+    override fun getNumberDice(): String {
+        val range = 1..number
+        val result = range.random()
+        return "В четырехгранном кубике выпало число $result"
+    }
+}
+
+class SixSidedDice(private val number: Int = 6): Dice(){
+    override fun getNumberDice(): String {
+        val range = 1..number
+        val result = range.random()
+        return "В шестигранном кубике выпало число $result"
+    }
+}
+
+class EightSidedDice(private val number: Int = 8): Dice(){
+    override fun getNumberDice(): String {
+        val range = 1..number
+        val result = range.random()
+        return "В восьмигранном кубике выпало число $result"
+    }
+}
