63ba7bf6d48292065eddd6830f2ebfd0be7bb331
romka <romka_gra4@mail.ru>
2023-07-20 23:40:45 +0500

    KS 18 - 2:  made abstract class "Dice"

--- src/main/kotlin/lesson_18/lesson_18_task_3.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_3.kt
@@ -2,9 +2,9 @@ package lesson_18
 
 fun main() {
 
-    val fox1 = Fox("Корица")
-    val dog1 = Dog("Хантер")
-    val cat1 = Cat("Имбирь")
+    val fox1 = Fox("Корица", "ягоды")
+    val dog1 = Dog("Хантер", "кости")
+    val cat1 = Cat("Имбирь", "рыб")
 
     println(fox1.sleep())
     println(fox1.eat())
@@ -20,37 +20,25 @@ fun main() {
     println(cat1.eat())
 }
 
-open class Tamagotchi {
-    open fun sleep() = ""
-    open fun eat() = ""
+abstract class Tamagotchi(val name: String, val food: String) {
+    open fun sleep() = "$name - спит"
+    open fun eat() = "$name - ест $food"
 }
 
-class Fox(private val name: String) : Tamagotchi() {
-    override fun sleep(): String {
-        return "Лиса $name - спит"
+class Fox(name: String, food: String) : Tamagotchi(name, food) {
+    override fun sleep() = "Лиса $name - спит"
+
+    override fun eat() = "$name - ест $food"
     }
 
-    override fun eat(): String {
-        return "Лиса $name - ест ягоды"
-    }
+class Dog(name: String, food: String) : Tamagotchi(name, food) {
+    override fun sleep() = "Собака $name - спит"
+
+    override fun eat() = "Собака $name - ест $food"
 }
 
-class Dog(private val name: String) : Tamagotchi() {
-    override fun sleep(): String {
-        return "Собака $name - спит"
-    }
+class Cat(name: String, food: String) : Tamagotchi(name, food) {
+    override fun sleep() = "Кот $name - спит"
 
-    override fun eat(): String {
-        return "Собака $name - ест кости"
-    }
-}
-
-class Cat(private val name: String) : Tamagotchi() {
-    override fun sleep(): String {
-        return "Кот $name - спит"
-    }
-
-    override fun eat(): String {
-        return "Кот $name - ест рыбу"
-    }
+    override fun eat() = "Кот $name - ест $food"
 }
