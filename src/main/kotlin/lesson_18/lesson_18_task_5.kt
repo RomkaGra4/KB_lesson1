ad40a1ddc5461525194e125c2ee308834835f4a1
romka <romka_gra4@mail.ru>
2023-07-22 00:33:51 +0500

    KS 18 - 5:  with corrections

--- src/main/kotlin/lesson_18/lesson_18_task_5.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_5.kt
@@ -1,8 +1,8 @@
 package lesson_18
 
 fun main() {
-    val circle1 = Circle(1, 3)
-    val square1 = Square(3,5)
+    val circle1 = Circle(1.5f, 3)
+    val square1 = Square(3,5.3f)
     val dot1 = Dot(2,9)
 
     println(circle1.draw())
@@ -11,31 +11,31 @@ fun main() {
 
 }
 
-abstract class Screen(private val x: Int, private val y: Int) {
+abstract class Screen(private val x: Any, private val y: Any) {
 
-    open fun draw() = ""
+    abstract fun draw(): String
 
 }
 
 class Circle(
-    private val x: Int,
-    private val y: Int,
+    private val x: Any,
+    private val y: Any,
     private val anObject: String = "Круг",
 ) : Screen(x, y) {
     override fun draw() = "Создан объект $anObject с координатами: x = $x, y = $y"
 }
 
 class Square(
-    private val x: Int,
-    private val y: Int,
+    private val x: Any,
+    private val y: Any,
     private val anObject: String = "Квадрат",
 ) : Screen(x, y) {
     override fun draw() = "Создан объект $anObject с координатами: x = $x, y = $y"
 }
 
 class Dot(
-    private val x: Int,
-    private val y: Int,
+    private val x: Any,
+    private val y: Any,
     private val anObject: String = "Точка",
 ) : Screen(x, y) {
     override fun draw() = "Создан объект $anObject с координатами: x = $x, y = $y"
