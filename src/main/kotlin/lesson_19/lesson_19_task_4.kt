c2a29547bcd7ceb0c9be0bf0e704ab036d6f2aad
romka <romka_gra4@mail.ru>
2023-08-15 22:31:52 +0500

    KS 19 - 4:  Completed task number 4 for lesson 19

--- src/main/kotlin/lesson_19/lesson_19_task_4.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_4.kt
@@ -1,4 +1,52 @@
 package lesson_19
 
 fun main(){
+    val tank = Tank()
+
+    tank.armamentAmmo(Ammunition.BLUE)
+    tank.shot(Ammunition.BLUE)
+    println()
+    tank.armamentAmmo(Ammunition.GREEN)
+    tank.shot(Ammunition.GREEN)
+    println()
+    tank.armamentAmmo(Ammunition.RED)
+    tank.shot(Ammunition.RED)
+
+}
+
+enum class Ammunition{
+    BLUE,
+    GREEN,
+    RED,
+}
+
+fun setAmmunition(ammo: Ammunition){
+    when(ammo){
+        Ammunition.BLUE -> println("Синие патроны")
+        Ammunition.GREEN -> println("Зеленые патроны")
+        Ammunition.RED -> println("Красные патроны")
+    }
+}
+
+class Tank{
+
+    fun armamentAmmo(ammo: Ammunition) {
+        print("Танк вооружился боеприпасами: ")
+        when(ammo){
+            Ammunition.BLUE -> setAmmunition(Ammunition.BLUE)
+            Ammunition.GREEN -> setAmmunition(Ammunition.GREEN)
+            Ammunition.RED -> setAmmunition(Ammunition.RED)
+        }
+    }
+
+    fun shot(ammo: Ammunition){
+        print("Танк выстрелил! Нанесенный урон: ")
+        when(ammo){
+            Ammunition.BLUE -> println(5)
+            Ammunition.GREEN -> println(10)
+            Ammunition.RED -> println(20)
+        }
+    }
+
+
 }
