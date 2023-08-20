7b239e7ae004ee8ce59bd5b25f59d9e8f3317b03
romka <romka_gra4@mail.ru>
2023-08-20 22:15:16 +0500

    KS 19 - 4: with corrections

--- src/main/kotlin/lesson_19/lesson_19_task_4.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_4.kt
@@ -1,23 +1,38 @@
 package lesson_19
 
 fun main(){
-    val tank = Tank()
+    val tank = Tank(null)
 
     tank.armamentAmmo(Ammunition.BLUE)
-    tank.shot(Ammunition.BLUE)
+    println(Ammunition.BLUE.shot())
     println()
-    tank.armamentAmmo(Ammunition.GREEN)
-    tank.shot(Ammunition.GREEN)
-    println()
-    tank.armamentAmmo(Ammunition.RED)
-    tank.shot(Ammunition.RED)
 
+    tank.armamentAmmo(Ammunition.GREEN)
+    println(Ammunition.GREEN.shot())
+    println()
+
+    tank.armamentAmmo(Ammunition.RED)
+    println(Ammunition.RED.shot())
 }
 
 enum class Ammunition{
-    BLUE,
-    GREEN,
-    RED,
+    BLUE {
+        override fun shot(): String {
+            return "Танк выстрелил! Нанесенный урон: 5"
+        }
+    },
+    GREEN {
+        override fun shot(): String {
+            return "Танк выстрелил! Нанесенный урон: 10"
+        }
+    },
+    RED {
+        override fun shot(): String {
+            return "Танк выстрелил! Нанесенный урон: 20"
+        }
+    };
+
+    abstract fun shot() : String
 }
 
 fun setAmmunition(ammo: Ammunition){
@@ -28,7 +43,7 @@ fun setAmmunition(ammo: Ammunition){
     }
 }
 
-class Tank{
+class Tank(val armament: String?){
 
     fun armamentAmmo(ammo: Ammunition) {
         print("Танк вооружился боеприпасами: ")
@@ -38,15 +53,4 @@ class Tank{
             Ammunition.RED -> setAmmunition(Ammunition.RED)
         }
     }
-
-    fun shot(ammo: Ammunition){
-        print("Танк выстрелил! Нанесенный урон: ")
-        when(ammo){
-            Ammunition.BLUE -> println(5)
-            Ammunition.GREEN -> println(10)
-            Ammunition.RED -> println(20)
-        }
-    }
-
-
 }
