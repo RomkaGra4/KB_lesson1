0be77a898b0fcfb872d3d648c273387faf40c5e8
romka <romka_gra4@mail.ru>
2023-08-22 22:01:39 +0500

    KS 19 - 4: with corrections (2)

--- src/main/kotlin/lesson_19/lesson_19_task_4.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_4.kt
@@ -3,54 +3,39 @@ package lesson_19
 fun main(){
     val tank = Tank(null)
 
-    tank.armamentAmmo(Ammunition.BLUE)
-    println(Ammunition.BLUE.shot())
-    println()
+    tank.installProjectile(Ammunition.BLUE)
+    println(tank.shot())
+    tank.installProjectile(Ammunition.RED)
+    println(tank.shot())
+    tank.installProjectile(Ammunition.GREEN)
+    println(tank.shot())
 
-    tank.armamentAmmo(Ammunition.GREEN)
-    println(Ammunition.GREEN.shot())
-    println()
-
-    tank.armamentAmmo(Ammunition.RED)
-    println(Ammunition.RED.shot())
 }
 
-enum class Ammunition{
-    BLUE {
-        override fun shot(): String {
-            return "Танк выстрелил! Нанесенный урон: 5"
-        }
-    },
-    GREEN {
-        override fun shot(): String {
-            return "Танк выстрелил! Нанесенный урон: 10"
-        }
-    },
-    RED {
-        override fun shot(): String {
-            return "Танк выстрелил! Нанесенный урон: 20"
-        }
-    };
-
-    abstract fun shot() : String
+enum class Ammunition(val damage: Int){
+    BLUE(5),
+    GREEN(10),
+    RED(20),
 }
 
-fun setAmmunition(ammo: Ammunition){
+fun setAmmunition(ammo: Ammunition): String {
     when(ammo){
         Ammunition.BLUE -> println("Синие патроны")
         Ammunition.GREEN -> println("Зеленые патроны")
         Ammunition.RED -> println("Красные патроны")
     }
+    return ammo.damage.toString()
 }
 
-class Tank(val armament: String?){
+class Tank(var armament: String?){
 
-    fun armamentAmmo(ammo: Ammunition) {
+    fun installProjectile(ammo: Ammunition) {
         print("Танк вооружился боеприпасами: ")
-        when(ammo){
+        armament = when(ammo){
             Ammunition.BLUE -> setAmmunition(Ammunition.BLUE)
             Ammunition.GREEN -> setAmmunition(Ammunition.GREEN)
             Ammunition.RED -> setAmmunition(Ammunition.RED)
         }
     }
+    fun shot() = "Танк выстрелил! Нанесенный урон: $armament"
 }
