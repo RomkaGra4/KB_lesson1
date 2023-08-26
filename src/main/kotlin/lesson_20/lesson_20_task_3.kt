b48b9a1dc1240bd08d257067d1f7b94a27f65684
romka <romka_gra4@mail.ru>
2023-08-26 19:51:02 +0500

    KS 20 - 3: with lambda

--- src/main/kotlin/lesson_20/lesson_20_task_3.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_3.kt
@@ -3,14 +3,16 @@ package lesson_20
 fun main() {
 
     val player1 = Player(false)
-    player1.openTheDoor()
 
+    player1.openTheDoor(player1.key)
 }
 
 class Player(val key: Boolean) {
 
-    fun openTheDoor(): Any = if (key == true)
-        println("Игрок открыл дверь")
-    else
-        println("Дверь заперта")
+    val openTheDoor: (Boolean) -> Any = {
+        if (key == true)
+            println("Игрок открыл дверь")
+        else
+            println("Дверь заперта")
+    }
 }
