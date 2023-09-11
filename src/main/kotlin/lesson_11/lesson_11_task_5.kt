0f86974b0004122b4bdeb8b2940ec7c896c33bed
romka <romka_gra4@mail.ru>
2023-09-12 00:12:17 +0500

    KS 11 - 5:

--- src/main/kotlin/lesson_11/lesson_11_task_5.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_5.kt
@@ -3,27 +3,28 @@ package lesson_11
 fun main() {
 
     val forum = Forum()
-    forum.createNewUser("КЛИЕНТ")
-    forum.createNewUser("СПРАВОЧНАЯ")
-    forum.createNewMessage("Здравствуйте, это аэропорт?")
-    forum.createNewMessage("Добрый день! Верно.")
-    forum.createNewMessage("Хочу узнать - рейс 'Москва - Актау' вылетает по расписанию?")
-    forum.createNewMessage("Сейчас проверим. Секунду... Да, без изменений!")
-    println()
+    forum.createNewUser(UserForum("КЛИЕНТ", 1))
+    forum.createNewUser(UserForum("СПРАВОЧНАЯ", 2))
+
+    forum.createNewMessage(1, MessageForum("Здравствуйте! Это аэропорт?"))
+    forum.createNewMessage(2, MessageForum("Добрый день! Верно."))
+    forum.createNewMessage(1, MessageForum("Хочу узнать - рейс 'Москва - Актау' вылетает по расписанию?"))
+    forum.createNewMessage(2, MessageForum("Сейчас проверим. Секунду... Да, без изменений!"))
+
     forum.printThread()
 
 }
 
 class Forum {
-    val users = mutableListOf<String>()
-    val messages = mutableListOf<String>()
+    val users = mutableListOf<UserForum>()
+    val messages = mutableListOf<MessageForum>()
 
-    fun createNewUser(userName: String) {
+    fun createNewUser(userName: UserForum) {
         users.add(userName)
-        println("Добавлен новый пользователь $userName")
+        println("Добавлен новый пользователь $userName.")
     }
 
-    fun createNewMessage(message: String) {
+    fun createNewMessage(authorId: Int, message: MessageForum) {
         messages.add(message)
     }
 
@@ -36,3 +37,6 @@ class Forum {
                 println("${users[1]}: ${messages[i]}")
     }
 }
+
+class UserForum(name: String, authorId: Int)
+class MessageForum(text: String)
