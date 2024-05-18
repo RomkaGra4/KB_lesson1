fa90cf2b84b1a4f69fc138e86bf350be40235b1b
romka <romka_gra4@mail.ru>
2024-05-18 23:39:11 +0500

    KS 21 - 5: with correction

--- src/main/kotlin/lesson_21/lesson_21_task_5.kt
+++ src/main/kotlin/lesson_21/lesson_21_task_5.kt
@@ -3,7 +3,7 @@ package lesson_21
 fun main() {
 
     val skill = mapOf(
-        ("Power" to 200),
+        ("Power" to 300),
         ("Speed" to 300)
     ).maxCategory()
 
@@ -11,13 +11,8 @@ fun main() {
 
 fun Map<String, Int>.maxCategory() {
 
-    var maxValue = 0
-    for (i in this)
-        if (i.value > maxValue)
-            maxValue = i.value
+    val skill = this.maxByOrNull { it.value }
 
-    val skill = this.filter { it.value == maxValue }
-
-    println(skill.keys)
+    println(skill?.key)
 
 }
