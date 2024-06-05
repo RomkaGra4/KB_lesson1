a7298fc11c4cde07896917ee7c2031bb3d06d816
romka <romka_gra4@mail.ru>
2024-06-06 00:23:02 +0500

    KS 22 - 4: with first corrections

--- src/main/kotlin/lesson_22/lesson_22_task_4.kt
+++ src/main/kotlin/lesson_22/lesson_22_task_4.kt
@@ -1,25 +1,24 @@
 package lesson_22
 
-fun main(){
+fun main() {
 
-    val object1 = ViewModel.MainScreenState("Данные...", true)
-    val object2 = object1.copy(isLoading = true)
+    val object1 = MainScreenState("Данные...", true)
+    val object2 = ViewModel(object1.copy(isLoading = true))
     object2.loadData()
 
 }
 
 class ViewModel(mainScreenState: MainScreenState) {
 
-    data class MainScreenState(
-        val data: String,
-        val isLoading: Boolean = false,
-    ) {
-        fun loadData() {
-            println(MainScreenState("...отсутствие данных"))
-            println(MainScreenState("...загрузка данных"))
-            println(MainScreenState("...наличие загруженных данных"))
-        }
+    fun loadData() {
+        println(MainScreenState("...отсутствие данных"))
+        println(MainScreenState("...загрузка данных"))
+        println(MainScreenState("...наличие загруженных данных"))
     }
 
-
 }
+
+data class MainScreenState(
+    val data: String,
+    val isLoading: Boolean = false,
+)
